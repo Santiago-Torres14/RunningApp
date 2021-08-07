1ff8dfa8c9834ba38992236ddb04f91644be57fe
Santiago-Torres14 <santiagoatorres14@gmail.com>
2021-08-07 18:26:24 -0500

    Setting up hilt and database DI

---
+++ app/src/main/java/santiago/academy/runningapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package santiago.academy.runningapp.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import santiago.academy.runningapp.db.RunningDatabase
+import santiago.academy.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class appModule(){
+
+    @Singleton
+    @Provides
+    fun provideRunningDataBase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(
+        db: RunningDatabase
+    ) = db.getRunDao()
+
+}
